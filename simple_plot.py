16eb6bf418037c4ca9e1152b8a015d2ae0d66956
mizgierna <68358220+mizgierna@users.noreply.github.com>
2020-07-17 02:40:44 +0000

    Add files via upload

---
+++ simple_plot.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+filename = "inflammation.csv"
+data = np.loadtxt(filename, delimiter=',')
+print(data)
+print('data dimensions', data.shape)
+
+fig = plt.figure(figsize=(8.,2.),facecolor="blue")
+
+axes1 = fig.add_subplot(1, 3, 1)
+axes2 = fig.add_subplot(1, 3, 2)
+axes3 = fig.add_subplot(1, 3, 3)
+
+axes1.set_ylabel('avg')
+axes1.plot(data.mean(axis=0))
+
+axes2.set_ylabel('maximum')
+axes2.plot(data.max(axis=0))
+
+axes3.plot(data.min(axis=0))
+axes3.set_ylabel('minimum')
+
+plt.show()
